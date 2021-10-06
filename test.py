9e98417607f3be3795f9dc6d64ccdfb2579f9e23
SAURABH SINGH <72145932+saurabhkingp@users.noreply.github.com>
2021-10-06 16:36:57 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,14 +5,15 @@ from tkinter.filedialog import askopenfile, asksaveasfile
 from typing import Collection
 import ruamel.yaml
 import json
+
 root =Tk()
 root.geometry('500x300')
-
+root.configure(bg = "#b1cee9")
 #Field name
-selectfile=Label(root,text="Select file")
-savefile=Label(root,text="Save file")
+selectfile=Label(root,text="Select a YAML file", bg='#b1cee9')
+savefile=Label(root,text="Save file to", bg='#b1cee9')
 
-#pacling fileds
+#placing fileds
 selectfile.grid(row=1,column=2,padx=5,pady=10)
 savefile.grid(row=2,column=2,padx=5,pady=10)
 
@@ -21,10 +22,14 @@ selectfilevalue=StringVar
 savefilevalue=StringVar
 
 # creating entry filed
-selectfileentry=Entry(root,textvariable=selectfilevalue)
-savefileentry=Entry(root,textvariable=savefilevalue)
+selectfileentry=Entry(root,textvariable=selectfilevalue, bd = 0,
+    bg = "#e4e4e4",
+    highlightthickness = 2)
+savefileentry=Entry(root,textvariable=savefilevalue, bd = 0,
+    bg = "#e4e4e4",
+    highlightthickness = 2)
 
-#packinf entry fields
+#packing in entry fields
 selectfileentry.grid(row=1,column=3)
 savefileentry.grid(row=2,column=3)
 
@@ -58,15 +63,16 @@ def clear():
     selectfileentry.delete(0,END)
     savefileentry.delete(0,END)
 
-Button(text="Parse",command=lambda:[convert(),popup()]).grid(row=3,column=3,padx=5,pady=10)
+ttk.Button(text="Convert",command=lambda:[convert(),popup()]).grid(row=3,column=3,padx=5,pady=10)
 
-Button(text="Browse",command=open_file).grid(row=1,column=4,pady=10,padx=5)
+ttk.Button(text="Browse",command=open_file).grid(row=1,column=5,pady=10,padx=5)
 
-Button(text="Browse",command=save_file_dir).grid(row=2,column=4,padx=5,pady=10)
+ttk.Button(text="Browse",command=save_file_dir).grid(row=2,column=5,padx=5,pady=10)
 
-Button(text="cancle",command=root.destroy).grid(row=3,column=5,padx=5,pady=10)
+ttk.Button(text="Cancel",command=root.destroy).grid(row=3,column=5,padx=5,pady=10)
 
-Button(text="Clear",command=clear).grid(row=3,column=4,padx=5,pady=10)
+ttk.Button(text="Clear",command=clear).grid(row=3,column=4,padx=5,pady=10)
 
 root.title("YAML to JSON converter")
+root.resizable(False, False)
 root.mainloop()
